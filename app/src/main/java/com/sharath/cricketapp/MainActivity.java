fb91936ac6146ffdf372dd4a2ad5dfbd59fef678
sharathgangadhar <sgangadh@easi.com>
2019-08-27 23:32:47 +0530

    modified as per requriment.

--- app/src/main/java/com/sharath/cricketapp/MainActivity.java
+++ app/src/main/java/com/sharath/cricketapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 
     TextView firstInnTarget,secondInnScoreTV, wickets,remBalls, winner;
     Button bolowingBtn;
-    int secondInnScore;
+    int secondInnScore, firstInnScore;
     int currentWickets;
     int currentBalls;
     int maxBalls = 30;
@@ -35,8 +35,8 @@ public class MainActivity extends Activity {
         remBalls = (TextView)findViewById(R.id.secondInnBalls);
         bolowingBtn = (Button)findViewById(R.id.bowling);
         winner = (TextView)findViewById(R.id.winner);
-
-        firstInnTarget.setText(""+RandomGenerator.firstInnTarget());
+        firstInnScore = RandomGenerator.firstInnTarget();
+        firstInnTarget.setText(toString(firstInnScore));
         bolowingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,75 +50,84 @@ public class MainActivity extends Activity {
 
 
     public void eachBolowing() {
-        if(maxBalls <=MyConstants.totalMaxBalls) {
 
-            //Toast.makeText(,"Valid Ball", Toast.LENGTH_SHORT).show();
-            if(RandomGenerator.secondInnScoreGen().equals("0")) {
-                //Dot Ball
-                maxBalls-=1;
-                totalBalLeft-=1;
-                remBalls.setText(toString(totalBalLeft));
+        if(secondInnScore < firstInnScore) {
+            if(maxBalls <=MyConstants.totalMaxBalls) {
 
-            } else if(RandomGenerator.secondInnScoreGen().equals("1")) {
-                maxBalls-=1;
-                secondInnScore+=1;
-                totalBalLeft-=1;
-                secondInnScoreTV.setText(toString(secondInnScore));
-                remBalls.setText(toString(totalBalLeft));
-
-
-            } else if(RandomGenerator.secondInnScoreGen().equals("2")) {
-
-                maxBalls-=1;
-                secondInnScore+=2;
-                totalBalLeft-=1;
-                secondInnScoreTV.setText(toString(secondInnScore));
-                remBalls.setText(toString(totalBalLeft));
-
-            } if(RandomGenerator.secondInnScoreGen().equals("3")) {
-                //Dot Ball
-                maxBalls-=1;
-                secondInnScore+=3;
-                totalBalLeft-=1;
-                secondInnScoreTV.setText(toString(secondInnScore));
-                remBalls.setText(toString(totalBalLeft));
-
-            } else if(RandomGenerator.secondInnScoreGen().equals("4")) {
-                maxBalls-=1;
-                secondInnScore+=4;
-                totalBalLeft-=1;
-                secondInnScoreTV.setText(toString(secondInnScore));
-                remBalls.setText(toString(totalBalLeft));
-
-            } else if(RandomGenerator.secondInnScoreGen().equals("6")) {
-
-                maxBalls-=1;
-                secondInnScore+=6;
-                totalBalLeft-=1;
-                secondInnScoreTV.setText(toString(secondInnScore));
-                remBalls.setText(toString(totalBalLeft));
-
-            } else if(RandomGenerator.secondInnScoreGen().equals("NB")) {
-                //maxBalls-=1;
-                secondInnScore+=1;
-                secondInnScoreTV.setText(toString(secondInnScore));
-                remBalls.setText(toString(totalBalLeft));
-                //totalBalLeft-=1;
-
-            } else if(RandomGenerator.secondInnScoreGen().equals("W")) {
-
-                if(currentWickets < totalWicket) {
-                    currentWickets +=1;
+                //Toast.makeText(,"Valid Ball", Toast.LENGTH_SHORT).show();
+                if(RandomGenerator.secondInnScoreGen().equals("0")) {
+                    //Dot Ball
+                    maxBalls-=1;
                     totalBalLeft-=1;
-                    wickets.setText(toString(currentWickets));
                     remBalls.setText(toString(totalBalLeft));
 
-                } else {
-                    //Winner
-                }
+                } else if(RandomGenerator.secondInnScoreGen().equals("1")) {
+                    maxBalls-=1;
+                    secondInnScore+=1;
+                    totalBalLeft-=1;
+                    secondInnScoreTV.setText(toString(secondInnScore));
+                    remBalls.setText(toString(totalBalLeft));
 
+
+                } else if(RandomGenerator.secondInnScoreGen().equals("2")) {
+
+                    maxBalls-=1;
+                    secondInnScore+=2;
+                    totalBalLeft-=1;
+                    secondInnScoreTV.setText(toString(secondInnScore));
+                    remBalls.setText(toString(totalBalLeft));
+
+                } if(RandomGenerator.secondInnScoreGen().equals("3")) {
+                    //Dot Ball
+                    maxBalls-=1;
+                    secondInnScore+=3;
+                    totalBalLeft-=1;
+                    secondInnScoreTV.setText(toString(secondInnScore));
+                    remBalls.setText(toString(totalBalLeft));
+
+                } else if(RandomGenerator.secondInnScoreGen().equals("4")) {
+                    maxBalls-=1;
+                    secondInnScore+=4;
+                    totalBalLeft-=1;
+                    secondInnScoreTV.setText(toString(secondInnScore));
+                    remBalls.setText(toString(totalBalLeft));
+
+                } else if(RandomGenerator.secondInnScoreGen().equals("6")) {
+
+                    maxBalls-=1;
+                    secondInnScore+=6;
+                    totalBalLeft-=1;
+                    secondInnScoreTV.setText(toString(secondInnScore));
+                    remBalls.setText(toString(totalBalLeft));
+
+                } else if(RandomGenerator.secondInnScoreGen().equals("NB")) {
+                    //maxBalls-=1;
+                    secondInnScore+=1;
+                    secondInnScoreTV.setText(toString(secondInnScore));
+                    remBalls.setText(toString(totalBalLeft));
+                    //totalBalLeft-=1;
+
+                } else if(RandomGenerator.secondInnScoreGen().equals("W")) {
+
+                    if(currentWickets < totalWicket) {
+                        currentWickets +=1;
+                        totalBalLeft-=1;
+                        wickets.setText(toString(currentWickets));
+                        remBalls.setText(toString(totalBalLeft));
+
+                    } else {
+                        //Winner
+                        if(firstInnScore > secondInnScore) {
+                            winner.setText("Team A is won by"+remBalls+"balls");
+                        }
+                    }
+
+                }
             }
+        } else {
+            winner.setText("Team B is won");
         }
+
     }
 
     private String toString(int secondInnScore) {
